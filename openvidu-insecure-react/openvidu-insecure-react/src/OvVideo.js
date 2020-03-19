054cc765b362a6fd9271160f47b4864304af9470
Gabe Berke-Williams <gabebw@gabebw.com>
2020-03-19 13:05:58 -0700

    Rewrite some things

--- openvidu-insecure-react/openvidu-insecure-react/src/OvVideo.js
+++ openvidu-insecure-react/openvidu-insecure-react/src/OvVideo.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class OpenViduVideoComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
 
-    constructor(props) {
-        super(props);
-        this.videoRef = React.createRef();
+  componentDidUpdate(props) {
+    if (props && !!this.videoRef) {
+      this.props.streamManager.addVideoElement(this.videoRef.current);
     }
+  }
 
-    componentDidUpdate(props) {
-        if (props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
+  componentDidMount() {
+    if (this.props && !!this.videoRef) {
+      this.props.streamManager.addVideoElement(this.videoRef.current);
     }
+  }
 
-    componentDidMount() {
-        if (this.props && !!this.videoRef) {
-            this.props.streamManager.addVideoElement(this.videoRef.current);
-        }
-    }
+  render() {
+    // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createMediaElementSource
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-    render() {
-        return <video autoPlay={true} ref={this.videoRef} />;
-    }
-
+    return <video autoPlay={true} ref={this.videoRef} />;
+  }
 }
--- openvidu-insecure-react/openvidu-insecure-react/src/UserVideoComponent.js
+++ openvidu-insecure-react/openvidu-insecure-react/src/UserVideoComponent.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import OpenViduVideoComponent from './OvVideo';
-import './UserVideo.css';
+import React, { Component } from "react";
+import OpenViduVideoComponent from "./OvVideo";
+import "./UserVideo.css";
 
-export default class UserVideoComponent extends Component {
+const UserVideoComponent = ({ streamManager }) => {
+  const nickname = JSON.parse(streamManager.stream.connection.data).clientData;
 
-    getNicknameTag() {
-        // Gets the nickName of the user
-        return JSON.parse(this.props.streamManager.stream.connection.data).clientData;
-    }
+  if (streamManager === undefined) {
+    return <div />;
+  } else {
+    return (
+      <div>
+        <div className="streamcomponent">
+          <OpenViduVideoComponent streamManager={streamManager} />
+          <div>
+            <p>{nickname}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
-    render() {
-        return (
-            <div>
-                {this.props.streamManager !== undefined ? (
-                    <div className="streamcomponent">
-                        <OpenViduVideoComponent streamManager={this.props.streamManager} />
-                        <div><p>{this.getNicknameTag()}</p></div>
-                    </div>
-                ) : null}
-            </div>
-        );
-    }
-}
+export default UserVideoComponent;
