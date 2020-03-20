967cd7818d44e202759f797b7219f517a6017d22
Gabe Berke-Williams <gabebw@gabebw.com>
2020-03-19 19:59:29 -0700

    mute

--- src/OvVideo.js
+++ src/OvVideo.js
@@ -3,38 +3,34 @@ import React, { Component } from "react";
 export default class OpenViduVideoComponent extends Component {
   constructor(props) {
     super(props);
+    this.videoRef = React.createRef();
+
     // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createMediaElementSource
     this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    this.videoRef = React.createRef();
+    this.gainNode = audioCtx.createGain();
+    this.gainNode.connect(audioCtx.destination);
+    // mute
+    this.gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
   }
 
   componentDidUpdate(props) {
     if (props && !!this.videoRef) {
       this.props.streamManager.addVideoElement(this.videoRef.current);
-      if(!this.source) {
-        console.log('updated');
-        this.connect();
-      }
     }
   }
 
-  connect(){
-    this.source = this.audioCtx.createMediaElementSource(this.videoRef.current);
-    const gainNode = this.audioCtx.createGain();
-    this.source.connect(gainNode);
-    gainNode.connect(this.audioCtx.destination);
-    console.log(this.audioCtx.destination);
-    // mute
-    gainNode.gain.value = 0;
+  connect() {
+    const source = this.audioCtx.createMediaElementSource(
+      this.videoRef.current
+    );
+    source.connect(this.gainNode);
   }
 
   componentDidMount() {
     if (this.props && !!this.videoRef) {
       this.props.streamManager.addVideoElement(this.videoRef.current);
-      console.log(this.videoRef.current);
-      if(!this.source) {
-        console.log('mounted');
-        this.connect()
+      if (!this.source) {
+        this.connect();
       }
     }
   }
